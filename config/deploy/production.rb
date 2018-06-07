12d0282d4dd61ad7d25c3a0848625f3d12990198
aravind-manoharan97 <aravind@codingmart.com>
2018-06-07 20:58:21 +0530

    production.rb fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,11 +5,11 @@ set :rails_env, :production
 set :normalize_asset_timestamps, %{public/images public/javascripts
 public/stylesheets}
 
-role :app, %w{ubuntu@52.15.123.39}
+role :app, %w{ubuntu@18.188.14.67}
 
 set :ssh_options, {
   keys: %w(/home/aravind/ArAvInD/WoRk/AWS/railsapp.pem),
   # keys: %w(/Users/bhuvanamalini/Documents/project_codingmart/dorak_pem_key/dorakholdingdevelopment.pem),
   # keys: %w(/home/kolanthai/Desktop/server/dorakholdingdevelopment.pem),
 # keys: %w(/Users/karthi/Documents/keys/dorakholdingdevelopment.pem),
-}
+}	
